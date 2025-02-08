1573b0031d6ef8a9afb5d3f300449e8e6f89b4ff
@hexlet/cli <support@hexlet.io>
2025-02-08 15:10:14 +0300

    submit java-advanced-ru/sync-primitives

---
+++ java-advanced-ru/sync-primitives/src/main/java/exercise/SafetyList.java
@@ -0,0 +1,27 @@
+package exercise;
+
+class SafetyList {
+    // BEGIN
+    private int[] list = new int[0];
+
+    public synchronized void add(int num) {
+        var extendedArr = Arrays.copyOf(list, list.length + 1);
+        int length = extendedArr.length;
+        extendedArr[length - 1] = num;
+        list = extendedArr;
+    }
+
+    public int get(int index) {
+        if (list.length == 0) {
+            return -1;
+        } else if (list.length >= index) {
+            return -1;
+        }
+        return list[index];
+    }
+
+    public int getSize() {
+        return list.length;
+    }
+    // END
+}
