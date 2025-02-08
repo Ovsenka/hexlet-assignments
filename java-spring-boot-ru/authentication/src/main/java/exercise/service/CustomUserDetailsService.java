8c517b6d69b8ffebc00878bec645049e2d300144
@hexlet/cli <support@hexlet.io>
2025-02-08 18:39:07 +0300

    submit java-spring-boot-ru/authentication

--- java-spring-boot-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
+++ java-spring-boot-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
@@ -1,10 +1,10 @@
 package exercise.service;
 
 import exercise.repository.UserRepository;
+import exercise.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
