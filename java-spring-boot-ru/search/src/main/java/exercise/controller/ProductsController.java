9e13cc73db93816de305963cabd4cb368d272756
@hexlet/cli <support@hexlet.io>
2025-02-08 17:13:25 +0300

    submit java-spring-boot-ru/search

--- java-spring-boot-ru/search/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-boot-ru/search/src/main/java/exercise/controller/ProductsController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import exercise.exception.ResourceNotFoundException;
 import exercise.repository.ProductRepository;
